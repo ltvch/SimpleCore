70b838b6e8eeab69b136d2fa5348e472ab0564d7
Eugen Litoffchenko <ltvch@yahoo.com>
2020-11-09 23:58:05 +0200

    Приложение полностью рабочее.

--- SimpleCore/wwwroot/js/site.js
+++ SimpleCore/wwwroot/js/site.js
@@ -1,9 +1,5 @@
 ﻿$(document).ready(function () {
 
-    //$('.datepicker').datepicker();
-
-    //$('#dp3').datepicker();
-
     $('#datetimepicker1').datetimepicker({
         format: 'DD/MM/YYYY'
     });
@@ -11,44 +7,6 @@
     $('#datetimepicker2').datetimepicker({
         format: 'DD/MM/YYYY',
         autoclose: true
-    });
-
-    $('#showSeat').click(function () {
-        var url = $('#seatModal').data('url');
-
-        $.get(url, function (data) {
-            $('#seatContainer').html(data);
-
-            $('#seatModal').modal('show');
-        });
-    });
-
-    $('#showEmployee').click(function () {
-        var url = $('#employeeModal').data('url');
-
-        //$('input.datepicker').Zebra_DatePicker();
-
-        $.get(url, function (data) {
-            $('#employeeContainer').html(data);
-
-            $('#employeeModal').modal('show');
-
-            //$('#datepicker-formats').Zebra_DatePicker({
-            //    format: 'd/m/Y'
-            //});
-
-            //$('#datepicker-formats2').Zebra_DatePicker({
-            //    format: 'd/m/Y'
-            //});
-            $('#datetimepicker1').datetimepicker({
-                format: 'DD/MM/YYYY'
-            });
-
-            $('#datetimepicker2').datetimepicker({
-                format: 'DD/MM/YYYY',
-                autoclose: true,
-            });
-        });
-    }); 
+    });  
 });
 
